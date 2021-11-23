0b544af48ba77788b2a46151457e5d8f8f55d3a6
Moh Amilin <amilinmoh99@gmail.com>
2021-11-23 23:47:48 +0700

    update : dashboard content

--- pages/member/dashboard.tsx
+++ pages/member/dashboard.tsx
@@ -167,7 +167,7 @@ function dashboard() {
             <div className="ps-lg-0">
                 <h2 className="text-4xl fw-bold color-palette-1 mb-30">Overview</h2>
                 <div className="top-up-categories mb-30">
-                    <p className="text-lg fw-medium color-palette-1 mb-14">Top Up Categories</p>
+                    <p className="text-lg fw-medium color-palette-1 mb-14">Kelas yang diikuti</p>
                     <div className="main-content">
                         <div className="row">
                             <div className="col-lg-4 ps-15 pe-15 pb-lg-0 pb-4">
@@ -310,14 +310,14 @@ function dashboard() {
                     </div>
                 </div>
                 <div className="latest-transaction">
-                    <p className="text-lg fw-medium color-palette-1 mb-14">Latest Transactions</p>
+                    <p className="text-lg fw-medium color-palette-1 mb-14">Riwayat Transaksi</p>
                     <div className="main-content main-content-table overflow-auto">
                         <table className="table table-borderless">
                             <thead>
                                 <tr className="color-palette-1">
-                                    <th className="text-start" scope="col">Game</th>
-                                    <th scope="col">Item</th>
-                                    <th scope="col">Price</th>
+                                    <th className="text-start" scope="col">Image</th>
+                                    <th scope="col">Kelas</th>
+                                    <th scope="col">Harga</th>
                                     <th scope="col">Status</th>
                                 </tr>
                             </thead>
